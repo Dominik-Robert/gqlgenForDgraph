7e16b5b2c6e5f798538d730ceeb0a637088d3dae
Dominik Robert <dominikrobert@gmx.de>
2021-08-12 21:51:52 +0200

    Added New type

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
-module github.com/dominik-robert/gqlgenForDgraph
+module github.com/dominik-robert/gqlgenForValidation
 
 go 1.16
+
+require (
+	github.com/99designs/gqlgen v0.13.0
+	github.com/tidwall/gjson v1.8.1 // indirect
+	github.com/tomwright/dasel v1.17.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,21 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 
 	"github.com/99designs/gqlgen/api"
 	"github.com/99designs/gqlgen/codegen/config"
 	"github.com/99designs/gqlgen/plugin/modelgen"
+	"github.com/tidwall/gjson"
 )
 
 // Defining mutation function
 func mutateHook(b *modelgen.ModelBuild) *modelgen.ModelBuild {
+	data, err := ioutil.ReadFile("validation.json")
+	rootNode := gjson.ParseBytes(data)
+
+	if err != nil {
+		log.Fatal("Error while loading file", err)
+	}
+
 	for _, model := range b.Models {
-		for _, field := range model.Fields {
-			if field.Name == "dType" {
-				field.Tag = `json:"dgraph.type,omitempty`
+		var childNode gjson.Result
+		if childNode = rootNode.Get(model.Name); childNode.Exists() {
+			for _, field := range model.Fields {
+				var node gjson.Result
+				if node = childNode.Get(field.Name); node.Exists() {
+					if node.IsObject() {
+						field.Tag += ` govalid:"`
+						node.ForEach(func(key, value gjson.Result) bool {
+							if value.String() == "" {
+								field.Tag += key.String() + "|"
+							} else {
+								field.Tag += key.String() + ":" + value.String() + "|"
+							}
+
+							return true
+						})
+						field.Tag += `"`
+					} else {
+						field.Tag += ` govalid:"` + node.String() + `"`
+					}
+				}
 			}
 		}
+
 	}
 
 	return b
